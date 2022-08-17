f627a584bf29ac2375e6b0d94c67c6f4b1aa2c3d
Rodrigo <roro.costa2018@gmail.com>
2022-08-17 17:19:57 -0300

    Atualização da msg de vitoria

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 const blocos = document.getElementsByTagName('div')
 const playAgain = document.getElementById('botao')
 const aviso = document.getElementById('p')
+const vencTxt = document.getElementById('vencedor')
+const finalMsg = document.querySelector('.msg')
+let vencedor;
 let jogador;
+
 
 const defineJogador = () =>{
     jogador = Math.round(Math.random()*1)
@@ -20,7 +24,6 @@ const ativarEvento= () =>{
         blocos[i].addEventListener('click',jogar)
     }
 }
-
 const desativarEvento = () => {
     for(let i = 0; i<blocos.length;i ++){
         blocos[i].removeEventListener('click',jogar)
@@ -30,48 +33,66 @@ const jogar = (bloco) =>{
     if(jogador == 0){
         bloco.target.textContent = 'X'
         jogador = 1
-        atualizarAviso()
-        resultado()
         document.getElementById(bloco.target.id).removeEventListener('click',jogar)
     }else  if(jogador == 1){
         bloco.target.textContent = 'O'
         jogador = 0
- 
-        atualizarAviso()   
-        resultado()
         document.getElementById(bloco.target.id).removeEventListener('click',jogar)
     }
+    atualizarAviso()
+    resultado()
+  
 }
-const resultado = ()=> {
+const declararVencedor = ()=>{
+    if(vencedor == 2){
+        vencTxt.textContent = 'O Vencedor é o jogador O!'
+    }else if(vencedor == 1){
+        vencTxt.textContent = 'O Vencedor é o jogador X!'
+    }else{
+        vencTxt.textContent = 'A partida resultou em empate!'
+    }
+    finalMsg.style.display='flex'
+}
+const resultado = () =>{
     if(
-        blocos[0].innerHTML == 'X' && blocos[1].innerHTML == 'X'&& blocos[2].innerHTML == 'X'||
-        blocos[3].innerHTML == 'X' && blocos[4].innerHTML == 'X'&& blocos[5].innerHTML == 'X'||  
-        blocos[6].innerHTML == 'X' && blocos[7].innerHTML == 'X'&& blocos[8].innerHTML == 'X'|| 
-        blocos[0].innerHTML == 'X' && blocos[4].innerHTML == 'X'&& blocos[8].innerHTML == 'X'||
-        blocos[6].innerHTML == 'X' && blocos[4].innerHTML == 'X'&& blocos[2].innerHTML == 'X'
-        ){
-        desativarEvento()
-        alert('O vencedor é o jogador: X')
-    }else if(
-        blocos[0].innerHTML == 'O' && blocos[1].innerHTML == 'O' && blocos[2].innerHTML == 'O'||
-        blocos[3].innerHTML == 'O' && blocos[4].innerHTML == 'O' && blocos[5].innerHTML == 'O'||  
-        blocos[6].innerHTML == 'O' && blocos[7].innerHTML == 'O' && blocos[8].innerHTML == 'O'|| 
-        blocos[0].innerHTML == 'O' && blocos[4].innerHTML == 'O' && blocos[8].innerHTML == 'O'||
-        blocos[6].innerHTML == 'O' && blocos[4].innerHTML == 'O' && blocos[2].innerHTML == 'O'
+        blocos[0].textContent == 'X' && blocos[1].textContent =='X' && blocos[2].textContent=='X'||
+        blocos[3].textContent == 'X' && blocos[4].textContent =='X' && blocos[5].textContent=='X'||
+        blocos[6].textContent == 'X' && blocos[7].textContent =='X' && blocos[8].textContent=='X'||
+        blocos[0].textContent == 'X' && blocos[4].textContent =='X' && blocos[8].textContent=='X'||
+        blocos[6].textContent == 'X' && blocos[4].textContent =='X' && blocos[2].textContent=='X'||
+        blocos[1].textContent == 'X' && blocos[4].textContent =='X' && blocos[7].textContent=='X'||
+        blocos[0].textContent == 'X' && blocos[3].textContent =='X' && blocos[6].textContent=='X'||    
+        blocos[2].textContent == 'X' && blocos[5].textContent =='X' && blocos[8].textContent=='X'   
     ){
+        vencedor = 1
+        declararVencedor()
         desativarEvento()
-        alert('O vencedor é o jogador: O')
     }else if(
-        blocos[0].innerHTML != '' && blocos[1].innerHTML != '' && blocos[2].innerHTML != ''&&
-        blocos[3].innerHTML != '' && blocos[4].innerHTML != '' && blocos[5].innerHTML != ''&&  
-        blocos[6].innerHTML != '' && blocos[7].innerHTML != '' && blocos[8].innerHTML != ''
+        blocos[0].textContent == 'O' && blocos[1].textContent =='O' && blocos[2].textContent=='O'||
+        blocos[3].textContent == 'O' && blocos[4].textContent =='O' && blocos[5].textContent=='O'||
+        blocos[6].textContent == 'O' && blocos[7].textContent =='O' && blocos[8].textContent=='O'||
+        blocos[0].textContent == 'O' && blocos[4].textContent =='O' && blocos[8].textContent=='O'||
+        blocos[6].textContent == 'O' && blocos[4].textContent =='O' && blocos[2].textContent=='O'||
+        blocos[1].textContent == 'O' && blocos[4].textContent =='O' && blocos[7].textContent=='O'||
+        blocos[0].textContent == 'O' && blocos[3].textContent =='O' && blocos[6].textContent=='O'||    
+        blocos[2].textContent == 'O' && blocos[5].textContent =='O' && blocos[8].textContent=='O'    
     ){
+        vencedor = 2
+        declararVencedor()
+        desativarEvento() 
+    }else if(  
+        blocos[0].textContent != '' && blocos[1].textContent !='' && blocos[2].textContent!=''&&
+        blocos[3].textContent != '' && blocos[4].textContent !='' && blocos[5].textContent!=''&&
+        blocos[6].textContent != '' && blocos[7].textContent !='' && blocos[8].textContent!=''
+    ){
+        declararVencedor()
         desativarEvento()
-        alert('O jogo empatou!')
     }
 }
 
 function inicia(){
+    finalMsg.style.display='none'
+    vencedor = 0
     defineJogador()
     ativarEvento()
     atualizarAviso()
@@ -82,3 +103,31 @@ function inicia(){
 }
 window.addEventListener('load',inicia)
 playAgain.addEventListener("click",inicia)
+
+/*const condicaoVitoria = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+]
+const checandoVitoria =  () =>{
+    if(condicaoVitoria.map(element => element.map(
+            inEl => {
+                blocos[inEl].textContent != ''
+            }
+        ))
+    ){
+        return element
+    }
+    if (element.every(element => element.textContent = 'X')){
+        alert('O Vencedor é o jogador X!')
+    }else if(element.every(element => element.textContent = 'O')){
+        alert('O Vencedor  o jogador O!')
+    }   
+
+}*/
+
